a2ae3a920be42848babf8e01f35b15a9a07af8ef
beardfriend <beardfriend21@gmail.com>
2022-07-24 23:06:07 +0900

    default color && splash logo

--- lib/app/common/config/app_colors.dart
+++ lib/app/common/config/app_colors.dart
@@ -10,4 +10,13 @@ class AppColors {
   final Color transparent = Colors.transparent;
   final Color black = Colors.black;
   final Color white = Colors.white;
+  final Color gray100 = Color(0xffEEEEEE);
+  final Color gray200 = Color(0xffCCCCCC);
+  final Color gray300 = Color(0xff999999);
+  final Color gray400 = Color(0xff6666666);
+  final Color gray600 = Color(0xff3333333);
+  final Color gray700 = Color(0xff1111111);
+  final Color main100 = Color(0xffB2FF34);
+  final Color deisionYes = Color(0xff52CEFA);
+  final Color deisionNo = Color(0xffF16464);
 }
